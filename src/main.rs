ae086f698daf8591d41d8839b8129575cf76a9b2
Omega65536 <oliver@pop-os.localdomain>
2023-02-09 08:33:48 +0100

    Add command-line argument

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,16 @@ mod environment;
 use crate::interpreter::Interpeter;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
-use std::fs;
+use std::{fs, env};
 
 fn main() {
-    let program = fs::read_to_string("example.salt").expect("Unable to read file!");
+    let args: Vec<String> = env::args().collect();
+    let source_file = args.get(1).expect("usage: salt <filename>");
+
+    let program = fs::read_to_string(source_file).expect("Unable to read file!");
 
     let mut lexer = Lexer::new(program.chars());
     let tokens = lexer.lex();
-    //println!("{:?}", tokens);
 
     let mut parser = Parser::new(tokens.iter());
     let ast = parser.parse();
